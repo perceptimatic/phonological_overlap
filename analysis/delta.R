db2d8b898ea857decd0fd8226b1868b80f124dc2
Ewan Dunbar <ewan.dunbar@utoronto.ca>
2025-02-12 19:01:38 -0500

    Refactor delta

--- analysis/delta.R
+++ analysis/delta.R
@@ -7,26 +7,38 @@ calculate_all_deltas <- function(d) {
   return(d)
 }
 
-read_delta <- function(f, layers=FALSE) {
-  readr::read_csv(f,
+read_delta <- function(f) {
+  raw <- readr::read_csv(f,
                   col_types=cols(TGT_first = col_logical(),
                                  TGT_first_code = col_number(),
                                  language_stimuli_code = col_number(),
-                                 .default = col_guess())) %>%
-    ({ if (layers) {
-      . %>%
+                                 .default = col_guess()))
+  raw_meta <- dplyr::select(raw, -contains("_distance_"))
+  raw_dist_layer <- dplyr::select(raw, matches("_layer[0-9]*_distance_"))
+  raw_dist_nolayer <- dplyr::select(raw, !matches("_layer[0-9]*_distance_") &
+                                      matches("_distance_"))
+  
+  long_layer <- NULL
+  long_nolayer <- NULL
+  if (ncol(raw_dist_layer) > 0) {
+    long_layer <- bind_cols(raw_meta, raw_dist_layer) %>%
       pivot_longer(cols=contains("_distance_"),
-                   names_to=c("Model", "Layer", "distance_type"),
-                   names_pattern="(.*)_layer([0-9]*)_(distance_.*)") %>%
-        mutate(Layer=as.numeric(Layer)) %>%
-        pivot_wider(names_from=distance_type)        
-    } else {
-      . %>%
-        pivot_longer(cols=contains("_distance_"),
-                     names_to=c("Model", "distance_type"),
-                     names_pattern="(.*)_(distance_.*)") %>%
-        pivot_wider(names_from=distance_type)   
-    } }) %>%
+                     names_to=c("Model", "Layer", "distance_type"),
+                     names_pattern="(.*)_layer([0-9]*)_(distance_.*)") %>%
+          mutate(Layer=as.numeric(Layer)) %>%
+          pivot_wider(names_from=distance_type)        
+  }
+  if (ncol(raw_dist_nolayer) > 0) {
+    long_nolayer <- bind_cols(raw_meta, raw_dist_nolayer) %>%
+          pivot_longer(cols=contains("_distance_"),
+                       names_to=c("Model", "distance_type"),
+                       names_pattern="(.*)_(distance_.*)") %>%
+          pivot_wider(names_from=distance_type)       
+  }
+
+  long <- bind_rows(long_layer, long_nolayer)
+    
+  long %>%
     calculate_all_deltas() %>%
     rename(`Δ Model`=`Δ`) %>%
     clean_discrimination_items()
